d412f8aa2f51b00e0ac707f55098776f0c878245
Saurav Shrestha <sauravxtha@gmail.com>
2019-12-26 15:47:52 +0545

    updated header title

--- inc/helpers.php
+++ inc/helpers.php
@@ -24,57 +24,73 @@ if ( ! function_exists( 'beatsmandu_shop_header_page_title' ) ) :
 	function beatsmandu_shop_header_page_title() {
 		if ( is_front_page() ) :
 			return;
-		elseif ( is_home() ) :
+		elseif ( is_home() || is_singular() ) :
 			?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading uppercase"><?php single_post_title(); ?></h1>
-        <p>The Music Maker WordPress Theme is a powerful website template that allows you to sell beats, music, loops, products, and more.</p>
-    </div>
+	<div class="container">
+		<h1 class="header-heading uppercase"><?php single_post_title(); ?></h1>
+	</div>
 </div>
-<?php
+			<?php
 		elseif ( is_archive() ) :
 			?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading uppercase"><?php the_archive_title(); ?></h1>
-    </div>
+	<div class="container">
+		<h1 class="header-heading uppercase"><?php the_archive_title(); ?></h1>
+	</div>
 </div>
-<?php
+			<?php
 
 elseif ( is_single() ) :
-    ?>
+	?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading uppercase"><?php printf( esc_html__( 'Single Blog', 'beatsmandu-shop' ), get_search_query() ); ?></h1>
-        <p>The Music Maker WordPress Theme is a powerful website template that allows you to sell beats, music, loops, products, and more.</p>
-    </div>
+	<div class="container">
+		<h1 class="header-heading uppercase"><?php esc_html__( 'Single Blog', 'beatsmandu-shop' ); ?></h1>
+	</div>
 </div>
-<?php
+	<?php
 		elseif ( is_search() ) :
 			?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading uppercase">
-            <?php printf( esc_html__( 'Search Results for: %s', 'beatsmandu-shop' ), get_search_query() ); ?>
-        </h1>
-    </div>
+	<div class="container">
+		<h1 class="header-heading uppercase">
+			<?php printf( esc_html__( 'Search Results for: %s', 'beatsmandu-shop' ), get_search_query() ); ?>
+		</h1>
+	</div>
 </div>
-<?php
+			<?php
 		elseif ( is_404() ) :
 			?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading uppercase">
-            <span><?php echo __( 'Oops!', 'beatsmandu-shop' ); ?></span><?php echo esc_html__( ' That page can&#39;t be found.', 'beatsmandu-shop' ); ?>
-        </h1>
+	<div class="container">
+		<h1 class="header-heading uppercase">
+			<span><?php echo __( 'Oops!', 'beatsmandu-shop' ); ?></span><?php echo esc_html__( ' That page can&#39;t be found.', 'beatsmandu-shop' ); ?>
+		</h1>
 
-        <div class="error-404 not-found">
-            <?php get_search_form(); ?>
-        </div>
-    </div>
+		<div class="error-404 not-found">
+			<?php get_search_form(); ?>
+		</div>
+	</div>
 </div>
-<?php
+			<?php
+		elseif ( is_page( 'my-account' ) ) :
+			?>
+<div class="page-content">
+	<div class="container">
+			<?php
+			if ( is_user_logged_in() ) {
+				?>
+		<h1 class="header-heading uppercase"><?php esc_html__( 'My Account', 'beatsmandu-shop' ); ?></h1>
+				<?php
+			} else {
+				?>
+		<h1 class="header-heading uppercase"><?php esc_html__( 'Register/Login', 'beatsmandu-shop' ); ?></h1>
+				<?php
+			}
+			?>
+	</div>
+</div>
+			<?php
 		endif;
 	}
 
