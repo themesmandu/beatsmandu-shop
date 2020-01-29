75dc74de022494516c36c9818d94b3a223c2f779
Saurav Shrestha <sauravxtha@gmail.com>
2020-01-29 17:45:42 +0545

    updated email

--- edd_templates/emails/footer-default.php
+++ edd_templates/emails/footer-default.php
@@ -2,63 +2,79 @@
 /**
  * Email Footer
  *
- * @author 		Easy Digital Downloads
- * @package 	Easy Digital Downloads/Templates/Emails
+ * @author      Easy Digital Downloads
+ * @package     Easy Digital Downloads/Templates/Emails
  * @version     2.1
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 
 // For gmail compatibility, including CSS styles in head/body are stripped out therefore styles need to be inline. These variables contain rules which are added to the template inline.
-$template_footer = "
-	border-top:0;
-	-webkit-border-radius:3px;
-";
+$template_footer = '
+    color: #9B9B9B;
+    font-size: 14px;
+    padding: 0 40px;
+';
 
-$credit = "
-	border:0;
-	color: #000000;
-	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
-	font-size:12px;
-	line-height:125%;
-	text-align:center;
-";
 ?>
 															</div>
 														</td>
-                                                    </tr>
-                                                </table>
-                                                <!-- End Content -->
-                                            </td>
-                                        </tr>
-                                    </table>
-                                    <!-- End Body -->
-                                </td>
-                            </tr>
+													</tr>
+												</table>
+												<!-- End Content -->
+											</td>
+										</tr>
+									</table>
+									<!-- End Body -->
+								</td>
+							</tr>
+
                             <tr>
-                                <td align="center" valign="top">
-                                    <!-- Footer -->
-                                    <table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer" style="<?php echo $template_footer; ?>">
-                                        <tr>
-                                            <td valign="top">
-                                                <table border="0" cellpadding="10" cellspacing="0" width="100%">
-                                                    <tr>
-                                                        <td colspan="2" valign="middle" id="credit" style="<?php echo $credit; ?>">
-                                                        <?php echo wpautop( wp_kses_post( wptexturize( apply_filters( 'edd_email_footer_text', '<a href="' . esc_url( home_url() ) . '">' . get_bloginfo( 'name' ) . '</a>' ) ) ) ); ?>
-                                                        </td>
+
+							<tr>
+								<td valign="top">
+									<!-- Footer -->
+									<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer" style="<?php echo $template_footer; ?>">
+										<tr>
+											<td valign="top">
+												<table border="0" cellpadding="10" cellspacing="0" width="100%">
+													<tr class="col-six mar-bot">
+                                                        <th><h4><?php echo esc_html__( 'Follow Us.', 'beatsmandu-shop' ); ?></h4></th>
+                                                        <th><h4><?php echo esc_html__( 'Contact Us.', 'beatsmandu-shop' ); ?></h4></th>
                                                     </tr>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                    <!-- End Footer -->
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </body>
+
+                                                    <tr class="col-six mar-bot">
+														<td id="credit" style="<?php echo $credit; ?>">
+															<span><?php echo esc_html__( 'We are always open to meet new people like you.', 'beatsmandu-shop' ); ?></span>
+														</td>
+
+														<td id="mail-to" style="<?php echo $mailto; ?>">
+														    <a href="mailto:hello@producerthemes.com"><?php echo esc_html__( 'hello@producerthemes.com', 'beatsmandu-shop' ); ?></a>
+														    <span><?php echo esc_html__( '© 2020 ', 'beatsmandu-shop' ); ?><?php echo wp_kses_post( wptexturize( apply_filters( 'edd_email_footer_text', '<a href="' . esc_url( home_url() ) . '">' . get_bloginfo( 'name' ) . '</a>' ) ) ); ?></span>
+														</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+
+							<tr>
+                                <td colspan="2" valign="middle" id="social" style="<?php echo $social; ?>">
+									<a href="https://www.facebook.com/ProducerThemes"><img src="https://www.producerthemes.com/wp-content/uploads/2020/01/facebook-gray.png" alt="facebook"></a>
+                                    <a href="https://www.instagram.com/producer.themes/"><img src="https://www.producerthemes.com/wp-content/uploads/2020/01/twitter-gray.png" alt="instagram"></a>
+                                    <a href="https://twitter.com/producerthemes"><img src="https://www.producerthemes.com/wp-content/uploads/2020/01/instagram-gray.png" alt="twitter"></a>
+								</td>
+							</tr>
+							<!-- End Footer -->
+						</table>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
 </html>
--- edd_templates/emails/header-default.php
+++ edd_templates/emails/header-default.php
@@ -12,78 +12,192 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 // For gmail compatibility, including CSS styles in head/body are stripped out therefore styles need to be inline. These variables contain rules which are added to the template inline. !important; is a gmail hack to prevent styles being stripped if it doesn't like something.
 $body = "
 	background-color: #f6f6f6;
-	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+	font-family: 'Montserrat', sans-serif;
+	line-height: 1.5;
+	box-sizing: border-box;
+
 ";
 $wrapper = "
 	width:100%;
 	-webkit-text-size-adjust:none !important;
 	margin:0;
-	padding: 70px 0 70px 0;
+	padding: 40px 0;
 ";
 $template_container = "
-	box-shadow:0 0 0 1px #f3f3f3 !important;
-	border-radius:3px !important;
+	padding: 42px 0px 42px;
 	background-color: #ffffff;
-	border: 1px solid #e9e9e9;
-	border-radius:3px !important;
-	padding: 20px;
 ";
-$template_header = "
-	color: #00000;
-	border-top-left-radius:3px !important;
-	border-top-right-radius:3px !important;
-	border-bottom: 0;
-	font-weight:bold;
-	line-height:100%;
-	text-align: center;
-	vertical-align:middle;
-";
+
 $body_content = "
-	border-radius:3px !important;
-	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
+display: block;
+	margin-bottom: 38px;
 ";
 $body_content_inner = "
 	color: #000000;
-	font-size:14px;
-	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
-	line-height:150%;
+	font-size: 14px;
 	text-align:left;
 ";
 $header_content_h1 = "
-	color: #000000;
-	margin:0;
-	padding: 28px 24px;
-	display:block;
-	font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif;
-	font-size:32px;
-	font-weight: 500;
-	line-height: 1.2;
+	line-height: 1.5;
+	margin: 0;
 ";
+
+$table_wrap = "
+	max-width: 600px;
+	margin: 0 auto;
+";
+
+$template_header_image = "
+	background-color: #000000;
+	padding: 30px 40px 31px;
+	text-align: center;
+";
+
+$template_header = "
+	text-align: center;
+";
+
 $header_img = edd_get_option( 'email_logo', '' );
 $heading    = EDD()->emails->get_heading();
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title><?php echo get_bloginfo( 'name' ); ?></title>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,800&display=swap" rel="stylesheet">
+		<style>
+			#template_header p {
+				font-size: 36px;
+    			font-weight: 800;
+    			color: #151515;
+				margin: 0 0 48px;
+				letter-spacing: -2px;
+				padding: 0 40px;
+			}
+
+			p,
+			table li {
+				font-size: 16px;
+    			font-weight: 400;
+    			color: #9B9B9B;
+				line-height: 1.5;
+				margin: 0;
+			}
+
+			table th {
+				padding: 0;
+				width: 50%;
+				text-align: left;
+			}
+
+			table tr td {
+				padding: 0;
+				vertical-align: top;
+			}
+
+			table li,
+			table li small {
+				font-size: 14px;
+			}
+
+			table a{
+				font-weight: 600;
+				color: #1595E7;
+				text-decoration: none;
+			}
+
+			table a:hover {
+				text-decoration: underline;
+			}
+
+			#social a {
+				margin-right: 15px;
+				line-height: 1;
+			}
+
+			#social img {
+				height: 20px;
+				width: 20px;
+			}
+
+			#social img:last-child {
+				margin: 0;
+			}
+
+			#template_footer h4 {
+				font-size: 18px;
+				font-weight: 600;
+				color: #1B1B1B;
+				padding: 0;
+				margin: 0 0 12px;
+			}
+
+			#template_footer span {
+				display: block;
+			}
+
+			.mar-bot a {
+				font-size: 14px;
+				display: block;
+				margin-bottom: 5px;
+			}
+
+			#social {
+				display: flex;
+				padding: 0 40px;
+				margin-top: 32px;
+			}
+
+			#template_body {
+				padding: 0 40px;
+				margin-bottom: 32px;
+			}
+
+			strong {
+				font-weight: 600;
+			}
+
+			#template_body a {
+				pointer-events: none;
+				color: #9B9B9B;
+				font-weight: 400;
+			}
+
+			#template_body ul a {
+				pointer-events: unset;
+				color: #1595E7;
+				font-weight: 600;
+			}
+
+			#mail-to span {
+				display: flex;
+			}
+
+			#mail-to span a{
+				margin: 0 0 0 5px;
+			}
+
+		</style>
 	</head>
 	<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="<?php echo $body; ?>">
 		<div style="<?php echo $wrapper; ?>">
-		<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+		<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="<?php echo $table_wrap; ?>">
 			<tr>
-				<td align="center" valign="top">
+				<td valign="top">
 					<?php if( ! empty( $header_img ) ) : ?>
-						<div id="template_header_image">
-							<?php echo '<img src="' . esc_url( $header_img ) . '" alt="' . get_bloginfo( 'name' ) . '" />'; ?>
+						<div id="template_header_image" style="<?php echo $template_header_image; ?>">
+							<?php echo '<img src="' . esc_url( $header_img ) . '" alt="' . get_bloginfo( 'name' ) . ' height="20px" width="286px" " />'; ?>
 						</div>
 					<?php endif; ?>
-					<table border="0" cellpadding="0" cellspacing="0" width="520" id="template_container" style="<?php echo $template_container; ?>">
+					<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_container" style="<?php echo $template_container; ?>">
 						<?php if ( ! empty ( $heading ) ) : ?>
 							<tr>
-								<td align="center" valign="top">
+								<td>
 									<!-- Header -->
-									<table border="0" cellpadding="0" cellspacing="0" width="520" id="template_header" style="<?php echo $template_header; ?>" bgcolor="#ffffff">
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header" style="<?php echo $template_header; ?>">
 										<tr>
 											<td>
 												<h1 style="<?php echo $header_content_h1; ?>"><?php echo $heading; ?></h1>
@@ -97,7 +211,7 @@ $heading    = EDD()->emails->get_heading();
 						<tr>
 							<td align="center" valign="top">
 								<!-- Body -->
-								<table border="0" cellpadding="0" cellspacing="0" width="520" id="template_body">
+								<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_body">
 									<tr>
 										<td valign="top" style="<?php echo $body_content; ?>">
 											<!-- Content -->
